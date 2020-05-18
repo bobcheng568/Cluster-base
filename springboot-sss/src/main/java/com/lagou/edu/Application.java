46bbf9b48a65fa0b609a1be0be041c2f98d266f4
baojun <baojun@xhqb.com>
2020-05-18 19:55:20 +0800

    生产war包改造

---
+++ springboot-sss/src/main/java/com/lagou/edu/Application.java
@@ -0,0 +1,22 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
